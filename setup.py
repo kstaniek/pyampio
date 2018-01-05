8e85caf733317569bc375f181cc3641308447c0a
Klaudiusz Staniek <kstaniek@gmail.com>
2018-01-05 16:58:35 +0100

    classifiers updated

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(name=pkg_name,
           'Topic :: Software Development :: Libraries :: Python Modules',
           'License :: OSI Approved :: Apache Software License',
           'Programming Language :: Python',
-          'Programming Language :: Python :: 3 :: Only'
+          'Programming Language :: Python :: 3 :: Only',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
